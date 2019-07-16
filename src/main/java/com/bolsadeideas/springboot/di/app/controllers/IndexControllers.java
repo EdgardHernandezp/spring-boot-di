349c75fc1e4e5f1758ccc5d01b2aa221330567c2
Dreamseeker5 <edgardhernandezp@gmail.com>
2019-07-16 09:43:55 -0400

    Created a interface to inject references through it.

--- src/main/java/com/bolsadeideas/springboot/di/app/controllers/IndexControllers.java
+++ src/main/java/com/bolsadeideas/springboot/di/app/controllers/IndexControllers.java
@@ -5,15 +5,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.bolsadeideas.springboot.di.app.models.service.IService;
 import com.bolsadeideas.springboot.di.app.models.service.MiServicio;
 
 @Controller
 public class IndexControllers {
 	
 	@Autowired
-	private MiServicio servicio;
+	private IService servicio;
 	
-	@GetMapping({"/","","index"})
+	@GetMapping({"/","","/index"})
 	public String index(Model model) {
 		model.addAttribute("objeto",servicio.operacion());
 		return "index";
--- src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicio.java
+++ src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicio.java
@@ -2,9 +2,10 @@ package com.bolsadeideas.springboot.di.app.models.service;
 
 import org.springframework.stereotype.Component;
 
-@Component
-public class MiServicio {
+@Component("MiServicioSimple")
+public class MiServicio implements IService{
 	
+	@Override
 	public String operacion() {
 		return "MAMENLO";
 	}
